adc68813f6487042ecf45aa3c68b2df1c56bf47a
tijn <tijn@tijn.dev>
2025-06-10 11:17:45 +0200

    refactor: move to bun & monojsx instead of elysiajs

--- src/index.tsx
+++ src/index.tsx
@@ -2,69 +2,67 @@ import { createGoogleGenerativeAI } from "@ai-sdk/google";
 import { generateText } from "ai";
 import DOMPurify from "isomorphic-dompurify";
 import { marked } from "marked";
-import { Html, html } from "@elysiajs/html";
-import { Elysia } from "elysia";
 
 const google = createGoogleGenerativeAI({
   apiKey: process.env.API_KEY,
 });
 
-const app = new Elysia();
+const server = Bun.serve({
+  port: 3000,
+  routes: {
+    "/": () => (
+      <html>
+        <head>
+          <meta charSet="UTF-8" />
+          <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+          />
+        </head>
+        <form action="/response">
+          <input
+            type="text"
+            required
+            name="prompt"
+            placeholder="Enter your prompt here"
+          />
+          <input type="submit" value="Submit" />
+        </form>
+      </html>
+    ),
+    "/response": async (req: Bun.BunRequest) => {
+      const prompt = new URL(req.url).searchParams.get("prompt");
 
-app.use(html());
+      if (!prompt) {
+        return Response.redirect("/", 301);
+      }
 
-app.get("/", () => (
-  <html>
-    <head>
-      <meta charset="UTF-8" />
-      <meta
-        name="viewport"
-        content="width=device-width, initial-scale=1.0"
-      />
-    </head>
-    <form action="/response">
-      <input
-        type="text"
-        required
-        name="prompt"
-        placeholder="Enter your prompt here"
-      />
-      <input type="submit" value="Submit" />
-    </form>
-  </html>
-));
+      const { text } = await generateText({
+        model: google("models/gemini-2.0-flash-exp"),
+        prompt,
+      });
 
-app.get("/response", async ({ query: { prompt }, redirect }) => {
-  if (!prompt) {
-    return redirect("/");
-  }
+      const dirtyHtml = await marked.parse(text);
+      const cleanHtml = DOMPurify.sanitize(dirtyHtml);
 
-  const { text } = await generateText({
-    model: google("models/gemini-2.0-flash-exp"),
-    prompt,
-  });
-
-  const dirtyHtml = await marked.parse(text);
-  const cleanHtml = DOMPurify.sanitize(dirtyHtml);
-
-  return (
-    <html>
-      <head>
-        <meta charset="UTF-8" />
-        <meta
-          name="viewport"
-          content="width=device-width, initial-scale=1.0"
-        />
-        <meta name="description" content={text} />
-        <title>{prompt}</title>
-      </head>
-      {cleanHtml}
-    </html>
-  );
+      return (
+        <html>
+          <head>
+            <meta charSet="UTF-8" />
+            <meta
+              name="viewport"
+              content="width=device-width, initial-scale=1.0"
+            />
+            <meta name="description" content={text} />
+            <title>{prompt}</title>
+          </head>
+          {html(cleanHtml)}
+        </html>
+      );
+    },
+  },
 });
 
-app.listen(3000);
-
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
+  `Server is running at ${server.hostname}:${server.port}`,
 );
